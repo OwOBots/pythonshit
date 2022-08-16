ed6acd1a2ff62de741cca7af2570578af077dc07
Reimu_bots <notyours066@gmail.com>
2022-08-16 01:44:06 -0700

    first attempt at a soundpost converter

---
+++ soundpostconvert.py
@@ -0,0 +1,46 @@
+# vars
+#import ffmpeg
+from curses import endwin
+from tkinter import E
+import filetype
+import subprocess
+import requests
+from urllib.parse import unquote
+from os.path import splitext
+from urllib.parse import urlparse
+video = input("the name of the webm= ")
+audio = input("audio url= ")
+audiodecoded = unquote(audio)
+r = requests.get(audiodecoded)
+head = r.content[:10]
+videogif = video
+codec = "copy"
+codecifgif = "libvpx-vp9"
+outputfile = input("what name do you want= ")
+
+
+def get_ext(url):
+    """Return the filename extension from url, or ''."""
+    parsed = urlparse(url)
+    root, ext = splitext(parsed.path)
+    return ext  # or ext[1:] if you don't want the leading '.'
+
+
+ifaudionotogg = get_ext(audiodecoded)
+
+if ifaudionotogg.endswith('.mp3'):
+    subprocess.run(
+        f"ffmpeg -i {video} -i {audiodecoded} -c:v {codec} {outputfile}")
+elif ifaudionotogg.endswith('.ogg'):
+    subprocess.run(
+        f"ffmpeg -i {video} -i {audiodecoded} -c {codec} {outputfile}")
+else:
+    print('somethings wrong')
+
+ifvideogif = videogif
+
+if ifvideogif.endswith('.gif'):
+    subprocess.run(
+        f"ffmpeg -i {video} -i {audiodecoded} -c {codecifgif} {outputfile}")
+else:
+    print("bruh")
