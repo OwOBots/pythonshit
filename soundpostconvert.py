f7c23b5c0cdec8bcde3a84e8efa8cbb93c2d192e
OwOBots <45722490+OwOBots@users.noreply.github.com>
2022-08-17 09:42:01 -0700

    Progressbar (#1)
    * woops
    
    * woops2
    
    * fuck python

--- soundpostconvert.py
+++ soundpostconvert.py
@@ -1,6 +1,8 @@
 # vars
 from curses import endwin
-from tkinter import E
+import os
+import sys
+from tkinter import E, Scale
 import subprocess
 import requests
 from urllib.parse import unquote
@@ -9,8 +11,12 @@ from urllib.parse import urlparse
 video = input("the name of the webm= ")
 audio = input("audio url= ")
 audiodecoded = unquote(audio)
-codec = "libvpx-vp9"
+codec = "copy"
+codemp3 = "libvpx-vp9"
 outputfile = input("what name do you want= ")
+#this is just used in the png checker
+#outputpng = input("what ext do you want= ")
+
 
 
 def get_ext(url):
@@ -20,20 +26,30 @@ def get_ext(url):
     return ext  # or ext[1:] if you don't want the leading '.'
 
 
+
+
+        
+
 ifaudionotogg = get_ext(audiodecoded)
 ifvideogif = video
+def audiostuff():
+    if ifaudionotogg.endswith('.mp3'):
+        subprocess.run(
+        f"ffmpeg -i {video} -i {audiodecoded} -c:v {codemp3} {outputfile}")
+    elif ifaudionotogg.endswith('.ogg'):
+        subprocess.run(
+        f"ffmpeg  -i {video} -i {audiodecoded} -c {codec} {outputfile}")
+    elif ifaudionotogg.endswith('.wav'):
+        subprocess.run(
+        f"ffmpeg -i {video} -i {audiodecoded} -c:v {codec} {outputfile}")
+    else:
+        print('somethings wrong')
+def videochecker():
+    if ifvideogif.endswith('.png'):
+        scale = input("format is x:x= ")
+        subprocess.run(
+        f"ffmpeg  -loop 1 -r 1 -i {video} -i {audiodecoded} -c:v {codemp3} -t 00:06:04 -pix_fmt yuv420p -vf scale={scale} {outputfile}")    
+    else:
+        audiostuff()
 
-if ifaudionotogg.endswith('.mp3'):
-    subprocess.run(
-        f"ffmpeg -i {video} -i {audiodecoded} -c:v {codec} {outputfile}")
-elif ifaudionotogg.endswith('.ogg'):
-    subprocess.run(
-        f"ffmpeg -i {video} -i {audiodecoded} -c {codec} {outputfile}")
-elif ifaudionotogg.endswith('.wav'):
-    subprocess.run(
-        f"ffmpeg -i {video} -i {audiodecoded} -c:v {codec} {outputfile}")
-elif ifvideogif.endswith('.gif'):
-    subprocess.run(
-        f"ffmpeg -r 1 -loop 1 -i {video} -i {audiodecoded}  -c:v {codec} {outputfile}")
-else:
-    print('somethings wrong')
+videochecker()
